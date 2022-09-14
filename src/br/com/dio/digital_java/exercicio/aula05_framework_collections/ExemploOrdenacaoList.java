9f5439bca38d1095b82ece594aadff4580373abe
d4u0ul <ahb@ecomp.poli.br>
2022-09-14 00:42:26 -0300

    Criação de lista para o Exemplo de ordenação de uma list - Implementação de Comparable e comparator

--- src/br/com/dio/digital_java/exercicio/aula05_framework_collections/ExemploOrdenacaoList.java
+++ src/br/com/dio/digital_java/exercicio/aula05_framework_collections/ExemploOrdenacaoList.java
@@ -2,6 +2,7 @@ package br.com.dio.digital_java.exercicio.aula05_framework_collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /*
@@ -28,10 +29,29 @@ public class ExemploOrdenacaoList {
         Collections.shuffle(meusGatos);
         System.out.println("Imprimindo na ordem alatória: "+ meusGatos);
 
-        System.out.println("Imprimindo na ordem natural: "+ meusGatos);
+        System.out.println("Imprimindo na ordem nominal: "+ meusGatos);
+        Collections.sort(meusGatos);//utilizando a interface comparable que é implementada na própria classe
+        System.out.println(meusGatos);
+
+        System.out.println("Imprimindo na ordem idade: "+ meusGatos);
+        Collections.sort(meusGatos, new ComparatorIdade());//utilizando a interface comparable que é implementada na própria classe
+        //ou tbm dá pra usar meusGatos.sort(new ComparatorIdade()):
+        System.out.println(meusGatos);
+
+        System.out.println("Imprimindo na ordem cor: "+ meusGatos);
+        Collections.sort(meusGatos, new ComparatorCor());//utilizando a interface comparable que é implementada na própria classe
+        //ou tbm dá pra usar meusGatos.sort(new ComparatorCor()):
+        System.out.println(meusGatos);
+
+        System.out.println("Imprimindo na ordem Nome/Cor/Idade: "+ meusGatos);
+        Collections.sort(meusGatos, new ComparatorNomeCorIdade());//utilizando a interface comparable que é implementada na própria classe
+        //ou tbm dá pra usar meusGatos.sort(new ComparatorNomeCorIdade()):
+        System.out.println(meusGatos);
+
+
     }
 }
-class Gato {
+class Gato implements Comparable<Gato>{
     private String nome;
     private Integer idade;
     private String cor;
@@ -61,5 +81,43 @@ class Gato {
                 ", idade=" + idade +
                 ", cor='" + cor + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Gato gato) {
+        /*
+        * ensinando a classe a indicar as comparações dos objetos
+        * para isso é preciso a classe implentar a interface comparator para ensinar o método sort como definir quem é maior, menor e igual(+,-,0)
+        * abaixo, é indicado que deve-se atribuir pelo nome do gato
+        * */
+        return this.getNome().compareToIgnoreCase(gato.getNome());
+    }
+}
+class ComparatorIdade implements Comparator<Gato>{
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+        return Integer.compare(o1.getIdade(), o2.getIdade());
+    }
+}
+class ComparatorCor implements Comparator<Gato>{
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+        /*Se o o1 for maior retorna +1, se for menor -1 e se forem iguais 0
+        * */
+        return (o1.getCor().compareToIgnoreCase(o2.getCor()));
+    }
+}
+
+class ComparatorNomeCorIdade implements Comparator<Gato>{
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+        int nome = o1.getNome().compareToIgnoreCase(o2.getNome());
+        if(nome !=0) return nome;
+        int cor = o1.getCor().compareToIgnoreCase(o2.getCor());
+        if(cor!=0) return cor;
+        return Integer.compare(o1.getIdade(), o2.getIdade());
     }
 }
